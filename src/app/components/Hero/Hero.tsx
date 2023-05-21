d8290632d94b62cda5ace0e64e4d7dc9709326a2
asahel_code <hoyx0101@gmail.com>
2023-05-21 09:55:13 +0100

    Tweak Hero

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <main className="h-[600px] bg-heroTwo flex justify-around w-full items-center">
+    <main className="h-[600px] bg-heroTwo flex justify-around w-full items-start">
         <div className="flex w-[466px] h-[401.24px] flex-col justify-center">
-            <p className="fontFamily-Poppins font-[600px]">Pet Store & Beyond</p>
-            <span>This is a sample landing page, created with Figma and Anima. No coding involved.</span>
+            <p className="w-[423.79px] font-poppins font-[600] text-[80px]">Pet Store & Beyond</p>
+            <span className="w-[466px] text-[#2D2D2D]">This is a sample landing page, created with Figma and Anima. No coding involved.</span>
             <div>
                 <button>Get Started</button>
             </div>
